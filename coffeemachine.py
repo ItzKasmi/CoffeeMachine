9fc1e87c28d531df0cf7fac9226d2c8ee90650b2
ItzKasmi <kasmi0328@gmail.com>
2024-02-29 20:28:19 -0500

    Base
    Baseline setup

---
+++ coffeemachine.py
@@ -0,0 +1,45 @@
+"""
+Make 3 hot flavors
+- Water
+- Coffee
+- Milk
+
+Use Coins
+- Penny: 1
+- Nickel: 5
+- Dime: 10
+- Quarter: 25
+"""
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
