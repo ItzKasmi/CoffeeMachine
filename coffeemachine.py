3bea3acc451bc9319d556e5b42600453e67418b2
ItzKasmi <kasmi0328@gmail.com>
2024-02-29 20:50:07 -0500

    Updated to split the menu and resources for easier viewing

--- coffeemachine.py
+++ coffeemachine.py
@@ -1,3 +1,5 @@
+from data import MENU, resources
+
 """
 Make 3 hot flavors
 - Water
@@ -11,35 +13,33 @@ Use Coins
 - Quarter: 25
 """
 
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+# Keep track of total money
+money = 0.0
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+# Loop to see if they want more coffee
+turn_off = False
+while not turn_off:
+    choice = input("What would you like? (Espresso/Latte/Cappuccino): ")
+    if choice.lower() == "end":
+        turn_off = True
 
+    elif choice.lower() == "report":
+        print(f'''
+Water: {resources["water"]}
+Milk: {resources["milk"]}
+Coffee: {resources["coffee"]}
+Money: ${money}
+'''
+        )
+
+    elif choice.lower() == "espresso":
+        pass
+
+    elif choice.lower() == "latte":
+        pass
+
+    elif choice.lower() == "cappuccino":
+        pass
+
+    else:
+        print("Invalid input")
